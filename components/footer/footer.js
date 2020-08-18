d5512a3cb6121a6bc2da4de9fd368a65526948f7
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-08-18 11:05:53 -0500

    Minor code improvements

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,18 +1,18 @@
 import styles from './footer.module.css';
 
 const Footer = ({}) => {
-    return (<div className={styles.footer}>
-            <footer className="footer">
-                <div className="content has-text-centered">
-                    <p>
-                        <strong> Parcial TGS </strong> por <a href="https://github.com/sergio1599/"> Sergio
-                        Quintana </a> Estudiante de Ingeniería de Sistemas de Sexto Semestre de la UPTC - Seccional
-                        Sogamoso.
-                    </p>
-                </div>
-            </footer>
-        </div>
-    )
-}
+  return (<div className={styles.footer}>
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+          <strong> Parcial TGS </strong> por <a href="https://github.com/sergio1599/"> Sergio
+          Quintana </a> Estudiante de Ingeniería de Sistemas de Sexto Semestre de la UPTC -
+          Seccional
+          Sogamoso.
+        </p>
+      </div>
+    </footer>
+  </div>);
+};
 
 export default Footer;
--- components/tabla/tabla.js
+++ components/tabla/tabla.js
@@ -1,28 +1,25 @@
 import useSWR from 'swr';
-import fetcher from "../../functions/fetcher";
-import styles from "./tabla.module.css";
-
+import fetcher from '../../functions/fetcher';
 
 const Tabla = () => {
-    const {data} = useSWR('/api/wordsearch', fetcher);
-    if (!data) {
-        return <p>Cargando sopa de letras </p>;
-    }
-    return (<>
-            <div className="table-container">
-                <table className={"table is-bordered is-striped is-narrow is-hoverable is-fullwidth"} border={1} >
-                    <tbody>
-                    {data.grid.map((row, i) => {
-                        return <tr key={i}>{row.map((col, j) => {
-                            return <td key={j}>{col}</td>
-                        })}</tr>
-                    })}
-                    </tbody>
-                </table>
-            </div>
-        </>
-    )
-}
-
+  const { data } = useSWR('/api/wordsearch', fetcher);
+  if (!data) {
+    return <p>Cargando sopa de letras</p>;
+  }
+  return (<>
+    <div className="table-container">
+      <table className={'table is-bordered is-striped is-narrow is-hoverable is-fullwidth'}
+             border={1}>
+        <tbody>
+          {data.grid.map((row, i) => {
+            return <tr key={i}>{row.map((col, j) => {
+              return <td key={j}>{col}</td>;
+            })}</tr>;
+          })}
+        </tbody>
+      </table>
+    </div>
+  </>);
+};
 
 export default Tabla;
