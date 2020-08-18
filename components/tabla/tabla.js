03e5e5eb9843ab3fcc764b3dfbac4fa6caa6fc7f
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-08-18 11:54:43 -0500

    Finish word search

--- components/tabla/tabla.js
+++ components/tabla/tabla.js
@@ -1,24 +1,85 @@
+import { useMemo, useState } from 'react';
 import useSWR from 'swr';
 import fetcher from '../../functions/fetcher';
+import styles from './tabla.module.css';
 
 const Tabla = () => {
+  const [selectedLetters, setSelectedLetters] = useState([]);
   const { data } = useSWR('/api/wordsearch', fetcher);
+
+  useMemo(() => {
+    setSelectedLetters([]);
+  }, [data]);
+
   if (!data) {
     return <p>Cargando sopa de letras</p>;
   }
+
+  const isSelected = (x, y) => {
+    for (let i = 0; i < selectedLetters.length; i++) {
+      const letter = selectedLetters[i];
+      if (letter[0] === x && letter[1] === y) return true;
+    }
+    return false;
+  };
+
+  const updateLetter = (letter) => {
+    if (!isSelected(letter[0], letter[1])) {
+      setSelectedLetters([...selectedLetters, letter]);
+    } else {
+      setSelectedLetters(selectedLetters.filter((it) => {
+        return !(it[0] === letter[0] && it[1] === letter[1]);
+      }));
+    }
+  };
+
+  const getSelectedWords = () => {
+    return Object.keys(data.placed || {}).map((it) => {
+      let count = 0;
+      data.placed[it].forEach((letter) => {
+        if (isSelected(letter[0], letter[1])) count += 1;
+      });
+      if (count >= data.placed[it].length) {
+        return it;
+      } else {
+        return null;
+      }
+    }).filter(it => it);
+  };
+
+  const showWordDescription = (word) => {
+    window.alert(data.wordsJson[word]);
+  };
+
   return (<>
     <div className="table-container">
-      <table className={'table is-bordered is-striped is-narrow is-hoverable is-fullwidth'}
-             border={1}>
+      <table
+        className={`table is-bordered is-striped is-narrow is-hoverable is-fullwidth ${styles.table}`}
+        border={1}>
         <tbody>
           {data.grid.map((row, i) => {
             return <tr key={i}>{row.map((col, j) => {
-              return <td key={j}>{col}</td>;
+              return <td
+                key={j}
+                className={isSelected(j, i) ? styles.selected : ''}
+                onClick={() => { updateLetter([j, i]); }}>
+                {col}
+              </td>;
             })}</tr>;
           })}
         </tbody>
       </table>
     </div>
+    <br/>
+    <p><b>Palabras:</b></p>
+    <ul className={styles.words}>
+      {Object.keys(data.placed || {}).map((it, i) => {
+        return (<li key={i} className={getSelectedWords().includes(it) ? styles.solved : ''}
+                    onClick={() => showWordDescription(it)}>
+          {it}
+        </li>);
+      })}
+    </ul>
   </>);
 };
 
--- pages/api/wordsearch.js
+++ pages/api/wordsearch.js
@@ -1,12 +1,24 @@
-import wordsearch from "../../functions/wordsearch";
+import wordsearch from '../../functions/wordsearch';
+import wordsJson from '../../functions/words.json';
 
-const words = ['node','sergio','daniel', 'jahir', 'arge', 'fernanda'];
-const size = 12;
+const shuffle = (array) => {
+  return array.sort(() => Math.random() - 0.5);
+};
 
 const getWordSearch = async (req, res) => {
-    const wordsearchs = await wordsearch(words, size, size);
-    return res.json(wordsearchs);
-}
+  const shuffledWords = shuffle([...Object.keys(wordsJson)]);
+  const words = shuffledWords
+    .slice(0, 10)
+    .sort((a, b) => {
+      const wordA = wordsJson[a];
+      const wordB = wordsJson[b];
+      return wordB.length - wordA.length;
+    });
+  const longestWord = words[0];
+  const size = Math.ceil(longestWord.length * 2);
+  const wordsearchs = await wordsearch(words, size, size);
+  return res.json({ ...wordsearchs, wordsJson: shuffledWords });
+};
 
 
 export default getWordSearch;
